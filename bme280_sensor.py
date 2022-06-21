e30d70bfed6beb9902e312974c7270aca1bd1a79
weyland-ai <49379261+weyland-ai@users.noreply.github.com>
2022-06-21 15:09:10 -0400

    Update bme280_sensor.py

--- bme280_sensor.py
+++ bme280_sensor.py
@@ -1,8 +1,8 @@
-import bme280
-import smbus2
-from time import sleep
+import bme280 # Provides a library for reading and interpreting Bosch BME280 environmental sensor data.  Reads temperature, humidity, and pressure.
+import smbus2 # used to interact with bme280 sensor
+# from time import sleep --> only necessary when working with single file
 
-port = 1
+port = 1 # 1 is part of the i2c file name: i2c-1.  If port is anything other than 1, an error will show saying: No such file: '/dev/i2c-2'   if port was = to 2
 address = 0x77 # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
 
